2194663a55096ef1df7649812b524fd831ced8bc
joshinii <joshini.mn@gmail.com>
2026-05-06 17:20:22 -0700

    Initial Commit

---
+++ GhostRunner/GhostRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostRunnerApp.swift
+//  GhostRunner
+//
+//  Created by Spartan on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct GhostRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhostRunnerTests/GhostRunnerTests.swift
@@ -0,0 +1,19 @@
+//
+//  GhostRunnerTests.swift
+//  GhostRunnerTests
+//
+//  Created by Spartan on 5/6/26.
+//
+
+import Testing
+@testable import GhostRunner
+
+struct GhostRunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
